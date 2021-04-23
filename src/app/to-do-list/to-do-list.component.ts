87afa76781f4a4be69a86d2ac7b26efd67c32bf4
castillonico <nico.a.castillo@gmail.com>
2021-04-23 03:55:52 -0300

    Se agregan elementos a la lista

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
   styleUrls: ['./to-do-list.component.css']
 })
-export class ToDoListComponent implements OnInit {
+export class ToDoListComponent { 
+  lista = [];  
+  
+  agregarTarea (laTarea) { 
+    this.lista.push ({
+      descripcion : laTarea.value, 
+      estado : false
+    }); 
+    console.log("agrego una tarea a la lista... "); 
+    console.log(this.lista)
+  } 
 
-  constructor() { }
+  quitarTarea () { 
+    console.log("quito la tarea... "); 
+  } 
 
-  ngOnInit(): void {
-  }
+
+
 
 }
