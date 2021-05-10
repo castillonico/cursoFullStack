e293fe389cc7960e881f050879014de38c292368
castillonico <nico.a.castillo@gmail.com>
2021-05-10 08:59:19 -0300

    Esqueleto completo ejercicio 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
 
-const routes: Routes = [];
+import { Pagina1Component } from './intranet/pagina1/pagina1.component';
+import { Pagina2Component } from './intranet/pagina2/pagina2.component';
+import { Pagina3Component } from './intranet/pagina3/pagina3.component';
+import { HomeComponent } from './public/home/home.component';
+import { LoginComponent } from './public/login/login.component';
+import { PaginaErrorComponent } from './public/pagina-error/pagina-error.component';
+
+const routes: Routes = [
+  { path: '**', component: PaginaErrorComponent }, 
+  { path: 'home', component: HomeComponent }, 
+  { path: 'login', component: LoginComponent }, 
+  { path: 'pagina1', component: Pagina1Component }, 
+  { path: 'pagina2', component: Pagina2Component }, 
+  { path: 'pagina3', component: Pagina3Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './public/login/login.component';
+import { HomeComponent } from './public/home/home.component';
+import { PaginaErrorComponent } from './public/pagina-error/pagina-error.component';
+import { Pagina1Component } from './intranet/pagina1/pagina1.component';
+import { Pagina2Component } from './intranet/pagina2/pagina2.component';
+import { Pagina3Component } from './intranet/pagina3/pagina3.component';
+import { HeaderComponent } from './public/header/header.component';
+import { FooterComponent } from './public/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PaginaErrorComponent,
+    Pagina1Component,
+    Pagina2Component,
+    Pagina3Component,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
